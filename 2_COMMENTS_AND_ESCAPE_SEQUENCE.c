89f325ec028fc102e4c3201ee47c2f11a22a8e0c
AMPADI BS <ambadibs108@gmail.com>
2024-12-26 23:12:41 +0530

    Add files via upload

---
+++ 2_COMMENTS_AND_ESCAPE_SEQUENCE.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+	//THIS IS A SINGLE LINE COMMENT
+
+	/* THESE IS A MULTI
+	LINE COMMENT*/
+
+	//new line
+	printf("HELLO WORLD \n ");
+	
+	//NEW TAB
+	printf("1 \t 2 \t 3\n 4 \t 5 \t 6 \n 7\t 8 \t 9 ");
+	return 0;
+}
---
+++ 3_VARIABLES.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+	//int 	:- integers (number from 0 to infinity)
+	//float :- float values (number with point values)
+	//char  :- characters (a-z words and numbers )
+
+	//declaration
+	int d;
+
+
+	//ineialization
+	int b = 10;
+
+	int y = 21;
+
+	float f = 21.1;
+
+	char a = 'a';
+
+	char s[]="string";
+
+	printf("NUMBER IS %d and %d \nFLOAT NUMBER IS %.2f\nCHARACTER IS %c\nSTRING IS %s",b,y,f,a,s);\
+	
+	return 0;
+}
